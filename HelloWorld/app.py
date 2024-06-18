e9f7c0e0e4ad40f0b100917f0ec9c91eb96479fb
Tom Fyfe <tomfyfe85@gmail.com>
2024-06-18 22:06:09 +0100

    up to lesson 22

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,9 +1,18 @@
-"""
-This module demonstrates some example functionality.
-"""
+"Scratch module for working through lessons"
 
-X = 1
-Y = 2
-UNIT_PRICE = 3
 
-print("hello")
+# def multiply(*list):
+#     total = 1
+#     for number in list:
+#         total *= number
+#     return total
+
+
+# print(multiply(2, 3, 4, 5))
+
+
+def save_user(**user):
+    print(user['id'])
+
+
+save_user(id=1, name="admin")
