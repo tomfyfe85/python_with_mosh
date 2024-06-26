8f72e821fdc02bbee3316b905f3218e98f4a1f2d
Tom Fyfe <tomfyfe85@gmail.com>
2024-06-26 15:21:25 +0100

    private members

--- Classes/custom_containers.py
+++ Classes/custom_containers.py
@@ -3,14 +3,30 @@
 
 class TagCloud:
     def __init__(self):
-        self.tags = {}
+        # prefix methods with __ to show it is private
+        self.__tags = {}
 
     def add(self, tag):
-        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+        "adds a tag and shows the number of tags"
+        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
+        print(self.__tags)
+
+    def __getitem__(self, tag):
+        "usage: print(cloud['python'])  # Output: 3"
+        return self.__tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.__tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.__tags)
+
+    def __iter__(self):  
+        return iter(self.__tags)
 
 
 cloud = TagCloud()
+
 cloud.add("Python")
 cloud.add("python")
-cloud.add("python")
-print(cloud.tags)
+cloud.add("rodeo")
