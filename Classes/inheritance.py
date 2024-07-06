5c69d55df896d688531c453404e5677143d04262
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-06 11:46:59 +0100

    classes - inheritence complete

--- Classes/inheritance.py
+++ Classes/inheritance.py
@@ -1,24 +1,34 @@
-class Mammal:
+"""
+This module defines the Animal class and its subclasses Mammal and Fish.
+"""
 
-    "LAND MAMMALS"
+
+class Animal:
+    "Parent class to Mammal, Fish"
+
+    def __init__(self):
+        self.age = 1
 
     def eat(self):
         "all Mammals eat"
         print("eat")
+
+
+class Mammal(Animal):
+    "Child class to Animal"
 
     def walk(self):
         "all land Mammals"
         print("walk")
 
 
-class Fish:
-
-    "ALL FISH"
-
-    def eat(self):
-        "all Mammals eat"
-        print("eat")
+class Fish(Animal):
+    "Child to animal"
 
     def swim(self):
         "all land Mammals"
         print("walk")
+
+
+m = Mammal()
+m.eat()
