854bb8407393d96a593932f8dff37e6c2deae527
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-29 21:22:06 +0100

    JSON

--- Modules/app.py
+++ Modules/app.py
@@ -59,3 +59,20 @@ print(path.suffix)
 #     reader = csv.reader(file)
 #     for row in reader:
 #         print(row)
+ 
+# JSON
+import json 
+from pathlib import Path
+
+# movies = [
+#     {"id": 1, "title":"Terminator", "year": 1984 },
+#     {"id": 2, "title":"Kindergarten Cop", "year": 1993 }
+# ]
+
+# data = json.dumps(movies)
+# Path("movies.json").write_text(data)
+
+# to read json
+data = Path("movies.json").read_text()
+movies = json.loads(data)
+print(movies[1]["title"])
