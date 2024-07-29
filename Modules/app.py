a6437bc92169cb57b4d0965f00b54940593337b8
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-29 21:03:53 +0100

    csv files

--- Modules/app.py
+++ Modules/app.py
@@ -1,6 +1,61 @@
-from ecommerce.shopping import sales
-
-
-
-
+# from ecommerce.shopping import sales
 # print(dir(sales))
+
+from pathlib import Path
+
+# pathlib - python docs
+path = Path("ecommerce/__init__.py")
+path.exists()
+path.is_file()
+path.is_dir()
+print(path.name)
+print(path.stem)
+print(path.suffix)
+
+# path.exists()
+# path.mkdir()
+# path.rmdir()
+# path.rename("ecommerce2")
+# print(path.iterdir())
+# for p in path.iterdir():
+#     print(p)
+
+# paths = [p for p in path.iterdir() if p.is_dir()]
+# print(paths)
+
+# py_files = [p for p in path.rglob("*.py")]
+# print(py_files)
+
+# from time import ctime
+
+# path = Path("ecommerce/__init__.py")
+# print(ctime(path.stat().st_ctime))
+
+# # zip files
+# from zipfile import ZipFile
+
+# # with ZipFile("files.zip", "w") as zip:
+# #     for path in Path("ecommerce").rglob("*.*"):
+# #         zip.write(path)
+
+# with ZipFile("files.zip") as zip:
+#     print(zip.namelist())
+#     info = zip.getinfo("ecommerce/__init__.py")
+#     print(info.file_size)
+#     print(info.compress_size)
+#     zip.extractall("extract")
+
+
+# CSVs
+# import csv
+
+# # with open("data.csv", "w") as file:
+# #     writer = csv.writer(file)
+# #     writer.writerow(["transaction_id", "product_id", "price"])
+# #     writer.writerow([100, 1, 5])
+# #     writer.writerow([100, 2, 15])
+    
+# with open("data.csv") as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
---
+++ Modules/extract/ecommerce/shopping/sales.py
@@ -0,0 +1,7 @@
+print("sales init", __name__)
+def calc_tax():
+    pass
+
+
+def calc_shipping():
+    pass
